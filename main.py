2e7ea97e544b6fb95cb80e9d15723839f354a0f9
Pablo-El-Camino <93223436+Pablo-El-Camino@users.noreply.github.com>
2022-04-04 22:54:02 +0300

    c4, add print at 8 row

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 
 def print_hi(name):
+    print('after c3')
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('after c1')
